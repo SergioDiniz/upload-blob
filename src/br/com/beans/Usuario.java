cba848141279ef8c5a9583a081142c211af72829
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-22 15:07:26 -0300

    bean

---
+++ src/br/com/beans/Usuario.java
@@ -0,0 +1,48 @@
+package br.com.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Usuario {
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@Lob
+	private byte[] foto;
+	
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public byte[] getFoto() {
+		return foto;
+	}
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+	
+	
+	
+}
